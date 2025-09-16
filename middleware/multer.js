4c5640bcdfca8e5dd1f3931319068ba54d205057
Janjancel <jancelk2y@gmail.com>
2025-09-16 10:11:24 +0800

    itemsdf

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,13 +1,30 @@
+// const multer = require("multer");
+// const path = require("path");
+
+// // store file temporarily in memory
+// const storage = multer.diskStorage({
+//   filename: (req, file, cb) => {
+//     cb(null, Date.now() + path.extname(file.originalname));
+//   },
+// });
+
+// const upload = multer({ storage });
+
+// module.exports = upload;
+
 const multer = require("multer");
 const path = require("path");
 
-// store file temporarily in memory
 const storage = multer.diskStorage({
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
   },
 });
 
-const upload = multer({ storage });
+// ⚡ no limit on file size
+const upload = multer({
+  storage,
+  limits: { fileSize: Infinity }, // effectively disables size limit
+});
 
 module.exports = upload;
