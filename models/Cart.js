9c6af0afc29fa0c0deb7fe135d98d45cc918f57c
Janjancel <jancelk2y@gmail.com>
2025-10-28 13:28:11 +0800

    image

--- models/Cart.js
+++ models/Cart.js
@@ -7,16 +7,16 @@
 
 // module.exports = mongoose.model("Cart", CartSchema);
 
-const mongoose = require("mongoose");
+  const mongoose = require("mongoose");
 
-const CartSchema = new mongoose.Schema({
-  userId: { type: String, required: true, unique: true },
-  cartItems: [
-    {
-      id: { type: String, required: true },
-      quantity: { type: Number, default: 1 },
-    }
-  ],
-});
+  const CartSchema = new mongoose.Schema({
+    userId: { type: String, required: true, unique: true },
+    cartItems: [
+      {
+        id: { type: String, required: true },
+        quantity: { type: Number, default: 1 },
+      }
+    ],
+  });
 
-module.exports = mongoose.model("Cart", CartSchema);
+  module.exports = mongoose.model("Cart", CartSchema);
